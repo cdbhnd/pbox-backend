86d177e80a9fd1afec2457d72e10fb678bda4531
Ognjen Tomic <gliamanti@gmail.com>
2016-12-12 12:05:48 +0100

    PB-25-folder-structure

---
+++ typings/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="globals/config/index.d.ts" />
+/// <reference path="globals/express/index.d.ts" />
+/// <reference path="globals/mongodb/index.d.ts" />
+/// <reference path="globals/serve-static/index.d.ts" />
+/// <reference path="modules/debug/index.d.ts" />
