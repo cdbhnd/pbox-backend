03725ded936e956417a534e8ac1db3e145a15a36
uros pajic <uros pajic>
2016-12-19 12:36:52 +0100

    Added CORS support

--- web/Server.ts
+++ web/Server.ts
@@ -1,14 +1,20 @@
 import * as express from 'express';
 import './controllers/';
 
-import {createExpressServer} from "routing-controllers";
+import {createExpressServer, useExpressServer} from "routing-controllers";
 
 export class Server {
     private app: express.Application;
 
     constructor() {
-        this.app = createExpressServer();
+        this.app = express();
+        this.app.use(function(req, res, next) {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Headers", "content-type");
+            next();
+        });
         this.app.use(express.static('assets'));
+        useExpressServer(this.app);
     }
 
     public listen(port: number) {
