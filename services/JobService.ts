a7df064d5f33521d2c7245780d1c5c7b678f34bf
filipStef92 <s.filip.92@gmail.com>
2017-01-10 15:44:25 +0100

    PB-59 Make sure that destination data is filled in before storing in DB, in progress

--- services/JobService.ts
+++ services/JobService.ts
@@ -1,16 +1,15 @@
 import { Types, kernel } from "../dependency-injection/";
-import {IJobService} from './IJobService';
+import { IJobService } from './IJobService';
 import * as Entities from '../entities/';
 import * as Repositories from '../repositories/';
-import {Check} from '../utility/Check';
+import { Check } from '../utility/Check';
 import * as Exceptions from '../exceptions/';
 import { injectable } from 'inversify';
 import * as Providers from '../providers/';
 
 @injectable()
-export class JobService implements IJobService 
-{
-    private  _jobRepository: Repositories.JobRepository;
+export class JobService implements IJobService {
+    private _jobRepository: Repositories.JobRepository;
     private _quoteProvider: Providers.IQuotesProvider;
     private _geocodeProvider: Providers.IGeocodeProvider;
     private _moment: any;
@@ -22,8 +21,7 @@ export class JobService implements IJobService
         this._moment = require('moment-timezone');
     }
 
-    public async createJob(job: Entities.Job): Promise<Entities.Job> 
-    {
+    public async createJob(job: Entities.Job): Promise<Entities.Job> {
         Check.notNull(job, 'job');
 
         let quote: Providers.Quote = await this._quoteProvider.getRandomQuote();
@@ -33,39 +31,33 @@ export class JobService implements IJobService
         }
 
         let gl: Entities.Geolocation = await this._geocodeProvider.reverse(job.pickup.latitude, job.pickup.longitude);
-        if (!!gl) 
-        {
+        if (!!gl) {
             job.pickup.address = gl.address;
         }
 
         job.status = Entities.JobStatuses.PENDING;
-        job.createdAt =  this._moment().format();
+        job.createdAt = this._moment().format();
 
         return await this._jobRepository.create(job);
     }
 
-    public async cancelJob(job: Entities.Job): Promise<Entities.Job>
-    {
+    public async cancelJob(job: Entities.Job): Promise<Entities.Job> {
         Check.notNull(job, 'job');
 
-        if (job.status == Entities.JobStatuses.ACCEPTED || job.status == Entities.JobStatuses.IN_PROGRESS) 
-        {
+        if (job.status == Entities.JobStatuses.ACCEPTED || job.status == Entities.JobStatuses.IN_PROGRESS) {
             job.status = Entities.JobStatuses.CANCELED;
 
             return await this._jobRepository.update(job);
-        } else 
-        {
-            throw new Exceptions.ServiceLayerException('CANCEL_JOB_FAILED_INVALID_STATUS');            
+        } else {
+            throw new Exceptions.ServiceLayerException('CANCEL_JOB_FAILED_INVALID_STATUS');
         }
     }
 
-    public async completeJob(job: Entities.Job): Promise<Entities.Job>
-    {
+    public async completeJob(job: Entities.Job): Promise<Entities.Job> {
         Check.notNull(job, 'job');
 
-        if (job.status != Entities.JobStatuses.IN_PROGRESS) 
-        {
-            throw new Exceptions.ServiceLayerException('COMPLETE_JOB_FAILED_INVALID_STATUS');            
+        if (job.status != Entities.JobStatuses.IN_PROGRESS) {
+            throw new Exceptions.ServiceLayerException('COMPLETE_JOB_FAILED_INVALID_STATUS');
         }
 
         job.status = Entities.JobStatuses.COMPLETED;
@@ -73,58 +65,78 @@ export class JobService implements IJobService
         return await this._jobRepository.update(job);
     }
 
-    public async updatePickup(job: Entities.Job, pickup: Entities.Geolocation): Promise<Entities.Job>
-    {
+    public async updatePickup(job: Entities.Job, pickup: Entities.Geolocation): Promise<Entities.Job> {
         Check.notNull(job, 'job');
         Check.notNull(pickup, 'pickup');
 
-        if (job.status != Entities.JobStatuses.ACCEPTED) 
-        {
-            throw new Exceptions.ServiceLayerException('PICKUP_UPDATE_FAILED_INVALID_STATUS');            
+        if (job.status != Entities.JobStatuses.ACCEPTED) {
+            throw new Exceptions.ServiceLayerException('PICKUP_UPDATE_FAILED_INVALID_STATUS');
         }
 
-        if(!job.pickup) 
-        {
+        if (!job.pickup) {
             job.pickup = {
                 latitude: null,
                 longitude: null,
                 address: null
             };
         }
-        job.pickup.latitude = pickup.latitude ? pickup.latitude : job.pickup.latitude; 
-        job.pickup.longitude = pickup.longitude ? pickup.longitude : job.pickup.longitude; 
-        job.pickup.address = pickup.address ? pickup.address : job.pickup.address; 
+        job.pickup.latitude = pickup.latitude ? pickup.latitude : job.pickup.latitude;
+        job.pickup.longitude = pickup.longitude ? pickup.longitude : job.pickup.longitude;
+        job.pickup.address = pickup.address ? pickup.address : job.pickup.address;
 
         return await this._jobRepository.update(job);
     }
 
-    public async updateDestination(job: Entities.Job, destination: Entities.Geolocation): Promise<Entities.Job>
-    {
+    public async updateDestination(job: Entities.Job, destination: Entities.Geolocation): Promise<Entities.Job> {
         Check.notNull(job, 'job');
         Check.notNull(destination, 'destination');
 
-        if (job.status != Entities.JobStatuses.ACCEPTED) 
-        {
-            throw new Exceptions.ServiceLayerException('DESTINATION_UPDATE_FAILED_INVALID_STATUS');            
+        console.log('job', job.destination);
+        console.log('destination', destination);
+
+        if (job.status != Entities.JobStatuses.ACCEPTED) {
+            throw new Exceptions.ServiceLayerException('DESTINATION_UPDATE_FAILED_INVALID_STATUS');
+        }
+        if (job.destination.address == "" || (job.destination.longitude == 0 || job.destination.longitude == 0)) {
+            throw new Exceptions.ServiceLayerException('DESTINATION_UPDATE_FAILED_CORDINATES_OR_ADDRESS_NOT_PROVIDED');
+        }
+        if (job.destination.address != destination.address) {
+            let c: Entities.Geolocation = await this._geocodeProvider.geocode(destination.address);
+            if (c) {
+                destination.latitude = c.latitude;
+                destination.longitude = c.longitude;
+            }
+        }
+        if ((job.destination.latitude != destination.latitude) && (job.destination.longitude != destination.longitude)) {
+            let a: Entities.Geolocation = await this._geocodeProvider.reverse(destination.latitude, destination.longitude);
+            if (a) {
+                destination.address = a.address;
+            }
         }
 
-        if(!job.destination) 
-        {
-            job.destination = {
-                latitude: null,
-                longitude: null,
-                address: null
-            };
-        }
-        job.destination.latitude = destination.latitude ? destination.latitude : job.destination.latitude; 
-        job.destination.longitude = destination.longitude ? destination.longitude : job.destination.longitude; 
-        job.destination.address = destination.address ? destination.address : job.destination.address; 
+        job.destination = destination;
 
         return await this._jobRepository.update(job);
     }
 
-    public async updateReceiver(job: Entities.Job, receiverName: string, receiverPhone: string): Promise<Entities.Job>
-    {
+    // private resolveGeolocation(geolocation: Entities.Geolocation): Entities.Geolocation 
+    // {
+    //     if (geolocation.address && (geolocation.latitude && geolocation.longitude)) {
+    //         return geolocation;
+    //     }
+
+    //     if (geolocation.address && (!geolocation.latitude || !geolocation.longitude)) {
+    //         let c: Entities.Geolocation = await this._geocodeProvider.geocode(geolocation.address);
+    //         if (c) {
+    //             geolocation.latitude = c.latitude;
+    //             geolocation.longitude = c.longitude;
+    //         }
+    //     }
+
+    //     return geolocation;
+    // }
+
+    public async updateReceiver(job: Entities.Job, receiverName: string, receiverPhone: string): Promise<Entities.Job> {
         Check.notNull(job, 'job');
 
         job.receiverName = receiverName ? receiverName : job.receiverName;
@@ -133,18 +145,15 @@ export class JobService implements IJobService
         return await this._jobRepository.update(job);
     }
 
-    public async assignCourier(job: Entities.Job, courier: Entities.User): Promise<Entities.Job>
-    {
+    public async assignCourier(job: Entities.Job, courier: Entities.User): Promise<Entities.Job> {
         Check.notNull(job, 'job');
         Check.notNull(courier, 'courier');
 
-        if (job.status != Entities.JobStatuses.PENDING) 
-        {
-            throw new Exceptions.ServiceLayerException('COURIER_ASSIGN_FAILED_INVALID_STATUS');            
+        if (job.status != Entities.JobStatuses.PENDING) {
+            throw new Exceptions.ServiceLayerException('COURIER_ASSIGN_FAILED_INVALID_STATUS');
         }
 
-        if (!!job.courierId) 
-        {
+        if (!!job.courierId) {
             throw new Exceptions.ServiceLayerException('COURIER_ASSIGN_FAILED_ALREADY_ASSIGNED');
         }
 
@@ -154,13 +163,11 @@ export class JobService implements IJobService
         return await this._jobRepository.update(job);
     }
 
-    public async unassignCourier(job: Entities.Job): Promise<Entities.Job>
-    {
+    public async unassignCourier(job: Entities.Job): Promise<Entities.Job> {
         Check.notNull(job, 'job');
 
-        if (job.status != Entities.JobStatuses.ACCEPTED) 
-        {
-            throw new Exceptions.ServiceLayerException('COURIER_UNASSIGN_FAILED_INVALID_STATUS');            
+        if (job.status != Entities.JobStatuses.ACCEPTED) {
+            throw new Exceptions.ServiceLayerException('COURIER_UNASSIGN_FAILED_INVALID_STATUS');
         }
 
         job.courierId = '';
@@ -169,14 +176,12 @@ export class JobService implements IJobService
         return await this._jobRepository.update(job);
     }
 
-    public async updateSize(job: Entities.Job, size: Entities.packageSize): Promise<Entities.Job>
-    {
+    public async updateSize(job: Entities.Job, size: Entities.packageSize): Promise<Entities.Job> {
         Check.notNull(job, 'job');
         Check.notNull(size, 'size');
 
-        if (job.status != Entities.JobStatuses.ACCEPTED) 
-        {
-            throw new Exceptions.ServiceLayerException('SIZE_UPDATE_FAILED_INVALID_STATUS');            
+        if (job.status != Entities.JobStatuses.ACCEPTED) {
+            throw new Exceptions.ServiceLayerException('SIZE_UPDATE_FAILED_INVALID_STATUS');
         }
 
         job.size = size;
@@ -184,19 +189,16 @@ export class JobService implements IJobService
         return await this._jobRepository.update(job);
     }
 
-    public async attachBox(job: Entities.Job, box: Entities.Box): Promise<Entities.Job>
-    {
+    public async attachBox(job: Entities.Job, box: Entities.Box): Promise<Entities.Job> {
         Check.notNull(job, 'job');
         Check.notNull(box, 'box');
 
-        if (job.status != Entities.JobStatuses.ACCEPTED) 
-        {
-            throw new Exceptions.ServiceLayerException('BOX_ATTACH_FAILED_INVALID_STATUS');            
+        if (job.status != Entities.JobStatuses.ACCEPTED) {
+            throw new Exceptions.ServiceLayerException('BOX_ATTACH_FAILED_INVALID_STATUS');
         }
 
-        if (!!job.box) 
-        {
-            throw new Exceptions.ServiceLayerException('BOX_ATTACH_FAILED_ALREADY_ATTACHED');            
+        if (!!job.box) {
+            throw new Exceptions.ServiceLayerException('BOX_ATTACH_FAILED_ALREADY_ATTACHED');
         }
 
         job.box = box.code;
