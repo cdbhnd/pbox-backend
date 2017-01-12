22c1030f5eebd8ade442f2f0ae51099a190d1372
uros pajic <uros pajic>
2017-01-12 16:55:57 +0100

    Fixed deploy script

--- deploy-ec2.sh
+++ deploy-ec2.sh
@@ -2,4 +2,4 @@ gulp &&
 gulp deploy &&
 ssh -i pbox-dev.pem ubuntu@ec2-35-156-247-206.eu-central-1.compute.amazonaws.com "rm -rf ~/apps/pbb; mkdir ~/apps/pbb" &&
 scp -r -i pbox-dev.pem deploy/* ubuntu@ec2-35-156-247-206.eu-central-1.compute.amazonaws.com:apps/pbb &&
-ssh -i pbox-dev.pem ubuntu@ec2-35-156-247-206.eu-central-1.compute.amazonaws.com "cd ~/apps/pbb; npm install"
+ssh -i pbox-dev.pem ubuntu@ec2-35-156-247-206.eu-central-1.compute.amazonaws.com "cd ~/apps/pbb; npm install; forever restartall"
