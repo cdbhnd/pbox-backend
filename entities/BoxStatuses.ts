e1966338a312ce75923bca4b6881ac26c07af256
uros pajic <uros pajic>
2017-01-30 13:28:36 +0100

    PB-89 box status chnages depends on the state of activator sensor

--- entities/BoxStatuses.ts
+++ entities/BoxStatuses.ts
@@ -2,6 +2,7 @@ export class BoxStatuses
 {
     public static ACTIVE = 'ACTIVE';
     public static IDLE = 'IDLE';
+    public static SLEEP = 'SLEEP';
     public static INOPERATIVE = 'INPOERATIVE';
   
 }
