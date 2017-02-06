f8bfcc2a3490114b7733e8a0eedfe787ec32f438
Ognjen Tomic <gliamanti@gmail.com>
2017-02-06 14:27:12 +0100

    PB-99 log records are sent to loggly

--- web/middleware/logMiddleware.ts
+++ web/middleware/logMiddleware.ts
@@ -1,43 +1,56 @@
-export async function logMiddleware(req: any, res: any, next: Function)
-{
+var winston = require('winston');
+require('winston-loggly-bulk');
+
+export async function logMiddleware(req: any, res: any, next: Function) {
     var oldWrite = res.write;
-        var oldEnd = res.end;
-        
-        var log = {
-            createdAt: new Date(),
-            request: {
-                headers: req.headers,
-                method: req.method,
-                url: req.url,
-                params: req.params,
-                query: req.query,
-                body: req.body
-            },
-            response: {
-                headers: null,
-                statusCode: null,
-                body: null
-            },
-        };
+    var oldEnd = res.end;
 
-        var chunks = [];
+    var log = {
+        createdAt: new Date(),
+        request: {
+            headers: req.headers,
+            method: req.method,
+            url: req.url,
+            params: req.params,
+            query: req.query,
+            body: req.body
+        },
+        response: {
+            headers: null,
+            statusCode: null,
+            body: null
+        },
+    };
 
-        res.write = function (chunk) {
+    var chunks = [];
+
+    res.write = function (chunk) {
+        chunks.push(chunk);
+
+        oldWrite.apply(res, arguments);
+    };
+
+    res.end = function (chunk) {
+        if (chunk)
             chunks.push(chunk);
 
-            oldWrite.apply(res, arguments);
-        };
+        log.response.body = Buffer.concat(chunks).toString('utf8');
+        log.response.headers = res._headers;
+        log.response.statusCode = res.statusCode;
 
-        res.end = function (chunk) {
-            if (chunk)
-                chunks.push(chunk);
+        //record to loggly
+        winston.add(winston.transports.Loggly, {
+            token: "e020724b-a477-40fc-994a-474dcd64107e",
+            subdomain: "pbox",
+            tags: ["Winston-NodeJS"],
+            json: true
+        });
 
-            log.response.body = Buffer.concat(chunks).toString('utf8');
-            log.response.headers = res._headers;
-            log.response.statusCode = res.statusCode;
-            console.log(log);
-            oldEnd.apply(res, arguments);
-        };
+        winston.log('info', log);
+        /////////////////////////////////////////////////////////
+        
+        oldEnd.apply(res, arguments);
+    };
 
-        next();
+    next();
 }
